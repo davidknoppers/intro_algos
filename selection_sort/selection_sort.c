407bd0ac763837b4dc97c8616f0f0a3c34dd634c
davidknoppers <david.knoppers@holbertonschool.com>
2016-11-17 23:09:02 +0000

    adding some documentation, removing extra vars, simplifying syntax

--- selection_sort/selection_sort.c
+++ selection_sort/selection_sort.c
@@ -1,19 +1,19 @@
 #include <stdio.h>
 
-int min(int a, int b)
-{
-	if (b < a)
-		return (b);
-	return (a);
-}
-
+/**
+ * selection_sort - n^2 num sorting algo
+ *
+ * @arr: input array
+ * size: size of input array
+ *
+ */
 void selection_sort(int arr[], int size)
 {
 	int i, j, min_index, temp;
-	for (i = 0; i < (size / sizeof(int)) -1 ; i++)
+	for (i = 0; i < size -1 ; i++)
 	{
 		min_index = i;
-		for (j = i + 1; j < (size / sizeof(int)); j++)
+		for (j = i + 1; j < size; j++)
 		{
 			if (arr[j] < arr[min_index])
 				min_index = j;
@@ -23,19 +23,24 @@ void selection_sort(int arr[], int size)
 		arr[min_index] = temp;
 	}
 }
+/**
+ * main - runs tests on selection sort
+ *
+ * Return: 0
+ */
 int main(void)
 {
 	int testarr[] = {1, 5, 6, 32, 2, 3, 43, 21, 4, 5, 2, 1, 7, 4, 3, 2,
 			 1, 4, 5, 8, 9, 12, 41,};
-	int size = sizeof(testarr);
-	int i, j;
+	int size = sizeof(testarr) / sizeof(int);
+	int i;
 	puts("Initial Array:");
-	for (i = 0; i < size / sizeof(int); i++)
+	for (i = 0; i < size; i++)
 		printf("%d ", testarr[i]);
 	puts("\n");
 	selection_sort(testarr, size);
 	puts("Final Array:");
-	for (i = 0; i < size / sizeof(int); i++)
+	for (i = 0; i < size; i++)
 		printf("%d ", testarr[i]);
 	puts("\n");
 	return(0);
